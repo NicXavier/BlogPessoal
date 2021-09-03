525b227325bbe7e1edf2e9c6c9a656ab16964573
NicXavier <notedenic@gmail.com>
2021-09-03 12:41:28 -0300

    Class BasicSecurityConfig

--- blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/security/BasicSecurityConfig.java
@@ -1,7 +1,9 @@
 package org.generation.blogPessoal.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +14,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity	
 public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 	
+	@Autowired
+	private UserDetailsServiceImplements service;
 	@Bean
 	public PasswordEncoder passwordenceoder()
 	{
@@ -30,6 +34,12 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
 		  	.and().cors()
 		  	.and().csrf().disable();
 		  	
+	}
+	
+	@Override
+	protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(service);
+		
 	}
 	
 }
--- blogPessoal/src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImplements.java
+++ blogPessoal/src/main/java/org/generation/blogPessoal/security/UserDetailsServiceImplements.java
@@ -9,8 +9,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.userdetails.UserDetails;
-
 
 @Service
 public class UserDetailsServiceImplements implements UserDetailsService {
